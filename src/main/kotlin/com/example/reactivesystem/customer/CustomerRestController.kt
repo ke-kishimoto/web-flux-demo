7279e307c4be59f2fbe03beaed1a00c36166b82b
k.kishimoto <k.kishimoto@tehno-nore.jp>
2025-08-04 11:56:29 +0900

    DBアクセスのリアクティブで画面を分ける

---
+++ src/main/kotlin/com/example/reactivesystem/customer/CustomerRestController.kt
@@ -0,0 +1,18 @@
+package com.example.reactivesystem.customer
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
+
+@RestController
+class CustomerRestController(
+    private val customerRepository: CustomerRepository
+) {
+    @GetMapping("/customers")
+    fun getAllCustomers(): Flux<Customer> {
+        return customerRepository.findAll()
+//            .buffer(100) // 3件ずつまとめて
+//            .delayElements(Duration.ofMillis(500)) // 1秒ごとに
+//            .flatMapIterable { it } // 再度1件ずつ流す
+    }
+}
